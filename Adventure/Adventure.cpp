b5c9aa1011286cbe737bdbee47e9fdb1e9a75980
FengXiao <1792198747@qq.com>
2024-12-13 09:30:37 +0800

    随机数(列表待更新)

--- Adventure/Adventure.cpp
+++ Adventure/Adventure.cpp
@@ -1,21 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int main()
-{
- 
-    printf("hello world");
-    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");    printf("hello world");
-    printf("hello world");
-    printf("hello world");
-    return 0;}
+#include<stdlib.h>
+#include<time.h>
+
+int randomdigit(){
+    time_t nowtime;
+    nowtime = time(NULL);//获取当前时间
+    srand((unsigned)nowtime);
+    return(rand()%10+1);
+    }//随机数函数产生一个1-10的随机数
+//--------------------------------------------------//
+
+
+
+
+
+
+int main(){
+    printf("%d",randomdigit());
+}
